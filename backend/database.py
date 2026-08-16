d0d300a8d9178dc9e0b421ca8bfa0f374ef49c63
mckuan <michellechienkuan@gmail.com>
2026-08-15 17:26:14 -0700

    fix: add to database url + fix tab icon/name

--- backend/database.py
+++ backend/database.py
@@ -13,7 +13,11 @@ DATABASE_URL = os.environ.get(
     "postgresql://postgres:postgres@localhost:5432/wvlength",
 )
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
